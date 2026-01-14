fa45b410d9410a73d5c70de1a490b3f577ebdba5
wuliten <bryandborst@gmail.com>
2026-01-14 18:18:11 -0500

    week2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "CEO";
+        job2._company = "Apple";
+        job2._startYear = 2026;
+        job2._endYear = 2050;
+
+
+        Resume bryanBorst = new Resume();
+        bryanBorst._personName = "Bryan Borst";
+        bryanBorst._jobs.Add(job1);
+        bryanBorst._jobs.Add(job2);
+
+        bryanBorst.DisplayResume();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,15 @@
+public class Resume
+{
+    public string _personName;
+    public List<Job> _jobs = new List<Job>();
+
+     public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+        foreach(Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
